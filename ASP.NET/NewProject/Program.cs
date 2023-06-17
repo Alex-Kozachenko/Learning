c9be903c5c929ebe74cbfc1d28f2f67b2439e671
DeadlockHoliday <119358312+DeadlockHoliday@users.noreply.github.com>
2023-06-17 07:26:45 +0300

    ASP.NET Razor

---
+++ ASP.NET/NewProject/Program.cs
@@ -0,0 +1,10 @@
+var connectionString = "";
+
+System.Console.WriteLine("HISADFADIFASDIA");
+
+public class Student 
+{
+	public Guid ID { get; set; }
+	public string? Name { get; set; }
+	public int Age { get; set; }
+}
